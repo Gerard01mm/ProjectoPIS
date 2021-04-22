1d6f9194930ddd6a50931888cbec10beef7d9886
EstelaPerezBassedas <eperezba14@alumnes.ub.edu>
2021-04-22 18:16:07 +0100

    Diseño formulario añadir reminders completo

--- app/src/main/java/com/example/my_notes/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/my_notes/ui/calendar/CalendarFragment.java
@@ -2,27 +2,38 @@ package com.example.my_notes.ui.calendar;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.my_notes.RecyclerView_adapters.RemindersAdapter;
 import com.example.my_notes.Reminders.Reminder;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -32,8 +43,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.my_notes.R;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 
 
 public class CalendarFragment extends Fragment {
@@ -50,6 +64,12 @@ public class CalendarFragment extends Fragment {
     private CalendarViewModel calendarViewModel;
     private Context parentContext;
     private RecyclerView reminder_list;
+    //Dialog
+    private ImageButton alarm;
+    private TextInputEditText nomReminder;
+    private TextInputEditText descriptionRem;
+    private TextView dateReminder;
+    private TextView dateCard;
 
     //Variables delr eminder
     private String title = null, description = null, date;
@@ -70,6 +90,7 @@ public class CalendarFragment extends Fragment {
 
         //Ajustem el calendari i afegim el event
         this.calendar = (MaterialCalendarView)root.findViewById(R.id.calendar);
+
         this.calendar.setSelectedDate(new CalendarDay());
         this.calendar.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
@@ -86,21 +107,50 @@ public class CalendarFragment extends Fragment {
         this.add_reminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder tit = new AlertDialog.Builder(getActivity());
-                tit.setTitle("Write a title for the reminder:");
 
-                final EditText input = new EditText(getActivity());
-                tit.setView(input);
+                AlertDialog.Builder tit = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+                View dialog = inflater.inflate(R.layout.reminder_form, null);
+                alarm = (ImageButton)dialog.findViewById(R.id.add_alarm_button);
+                nomReminder = (TextInputEditText) dialog.findViewById(R.id.dialog_title);
+                descriptionRem = (TextInputEditText) dialog.findViewById(R.id.dialog_description);
+                dateReminder = (TextView) dialog.findViewById(R.id.horaSelected);
+
+                tit.setTitle("Write a title for the reminder:");
+                tit.setView(dialog);
+
+
+                alarm.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("Entro aqui", "JAJAS");
+                        Calendar mCalendar = Calendar.getInstance();
+                        TimePickerDialog timePickerDialog =
+                                new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                                        mCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                        mCalendar.set(Calendar.MINUTE, minute);
+                                        String time = DateFormat.getTimeInstance(DateFormat.SHORT).format(mCalendar.getTime());
+                                        Log.d("MainActivity", "Selected time is " + time);
+                                    }
+                                }, mCalendar.get(Calendar.HOUR_OF_DAY), mCalendar.get(Calendar.MINUTE), true);
+                        timePickerDialog.show();
+                        Log.d("CAlendario time", String.valueOf(mCalendar.getTime()));
+                        date = mCalendar.getTime().toString();
+                        dateReminder.setText(date);
+                    }
+                });
 
                 tit.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        title = input.getText().toString();
-                        System.out.println(title);
-                        description = "Primera reminder";
+                        title = nomReminder.getText().toString();
+                        description = descriptionRem.getText().toString();
                         if(title != null && description != null){
                             String day = calendar.getSelectedDate().toString();
-
                             //Reminder nR = new Reminder(title, description, day);
                             calendarViewModel.addReminder(title, description, "", day, "", "");
                         }
@@ -112,11 +162,9 @@ public class CalendarFragment extends Fragment {
                         dialog.cancel();
                     }
                 });
-
                 tit.show();
             }
         });
-
         setLiveDataObservers();
         return root;
     }
@@ -145,4 +193,5 @@ public class CalendarFragment extends Fragment {
         calendarViewModel.getReminders().observe(getViewLifecycleOwner(), observer);
         calendarViewModel.getToast().observe(getViewLifecycleOwner(), observerToast);
     }
+
 }
